a65c8d41b3c3ddef17f797e088a65b68841ab056
MhammadRezaHunTeR <mrezasli123@gmail.com>
2022-07-03 16:59:01 +0430

    My  Second Web
    Test

---
+++ _pgbackup/_pgexport/header_1656674842.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!--  Converted from HTML to WordPress with Pinegrow Web Editor. https://pinegrow.com  -->
+<html dir="rtl" <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>@font-face { font-family: lalezarz; src: url('dist/webfonts/Lalezar-Regular.ttf'); } @font-face { font-family: gandom; src: url('dist/webfonts/gandom.ttf'); } h1 , h2 , h3 , h4 , h5 { font-family: lalezarz !important; } p , ul , li { font-family: gandom !important; } .site-header-inner { float: left; } .button-block , .footer-copyright { font-family: gandom !Important; }</style>
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+        <meta content="Pinegrow Web Editor" name="generator">
+        <?php wp_head(); ?>
+    </head>
+    <body class="is-boxed has-animations <?php echo implode(' ', get_body_class()); ?>">
+        <?php if( function_exists( 'wp_body_open' ) ) wp_body_open(); ?>
---
+++ _pgexport/_pgexport/header.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<?php if( function_exists( 'wp_body_open' ) ) wp_body_open(); ?>
+
+<?php get_footer(); ?>
